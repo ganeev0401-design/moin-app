e867d91c331a8a2a54ec8dcb456ddcc88ffc1ae5
Artem Ganeev <ganeev0401@gmail.com>
2026-06-27 15:19:14 +0500

    fix api route

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import "./globals.css";
 import { Inter } from "next/font/google";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,7 +11,15 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+
+        {/* 🚀 Telegram WebApp SDK */}
+        <Script
+          src="https://telegram.org/js/telegram-web-app.js"
+          strategy="beforeInteractive"
+        />
+      </body>
     </html>
   );
 }
